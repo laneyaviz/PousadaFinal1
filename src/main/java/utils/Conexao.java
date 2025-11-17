29ab76cfe610f87880a0e460c94037548f82e5b0
Laney Aviz <laneyaviz@gmail.com>
2025-11-17 00:41:59 -0300

    atualizar utils

--- src/main/java/utils/Conexao.java
+++ src/main/java/utils/Conexao.java
@@ -14,19 +14,23 @@ public class Conexao {
         }
 
         try {
-            // Lê as variáveis do Render
             String url = System.getenv("DB_URL");
             String user = System.getenv("DB_USER");
             String pass = System.getenv("DB_PASS");
+
+            System.out.println("URL recebida: " + url);
+            System.out.println("USER recebido: " + user);
 
             if (url == null || user == null || pass == null) {
                 throw new RuntimeException("Variáveis de ambiente não encontradas!");
             }
 
+            Class.forName("com.mysql.cj.jdbc.Driver");
             conexao = DriverManager.getConnection(url, user, pass);
+
             return conexao;
 
-        } catch (SQLException e) {
+        } catch (Exception e) {
             throw new RuntimeException("Erro ao conectar com o banco: " + e.getMessage(), e);
         }
     }
